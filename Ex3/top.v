a3e28ab5bace0411783c087b3e3357d37ead2576
DS-official <66016564+DS-official@users.noreply.github.com>
2020-06-08 16:50:33 +0100

    trial_1 ex3

--- Ex3/top.v
+++ Ex3/top.v
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Exercise #3 
+// Exercise #3
 // Student Name:
-// Date: 
+// Date:
 //
-//  Description: In this exercise, you need to design an up / down counter, where 
+//  Description: In this exercise, you need to design an up / down counter, where
 //  if the rst=1, the counter should be set to zero. If enable=0, the value
 //  should stay constant. If direction=1, the counter should count up every
 //  clock cycle, otherwise it should count down.
@@ -19,12 +19,36 @@
 `timescale 1ns / 100ps
 
 module counter(
-    //Todo: add ports 
-
+    //Todo: add ports
+    input rst,
+    input enable,
+    input clk,
+    input direction,
+    output [7:0] counter_out
     );
-                    
+
     //Todo: add registers and wires, if needed
+    reg [7:0] counter_out;
+
+    //initial value of counter_out
+    initial begin
+    counter_out = 8'b0;
+    end
 
     //Todo: add user logic
-      
+    always @ (posedge clk) begin
+    if(rst)
+        counter_out <= 8'b0;
+    else
+        if(!enable)
+            counter_out <= counter_out;
+        else
+            if(direction)
+                counter_out <= counter_out + 8'b1;
+            else
+                counter_out <= counter_out - 8'b1;
+                
+    end
+
+
 endmodule
--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -1,28 +1,72 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #3 - Up/Down counter
 // Student Name:
-// Date: 
+// Date:
 //
 // Description: A testbench module to test Ex3 - counter
-// Guidance: start with simple tests of the module (how should it react to each 
-// control signal?). Don't try to test everything at once - validate one part of 
+// Guidance: start with simple tests of the module (how should it react to each
+// control signal?). Don't try to test everything at once - validate one part of
 // the functionality at a time.
 //////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns / 100ps
 
 module top_tb(
     );
-    
-//Todo: Parameters
 
-//Todo: Regitsers and wires
+    //Todo: Parameters
+    parameter CLK_PERIOD = 10;
 
-//Todo: Clock generation
+    //Todo: Regitsers and wires
+    reg clk;
+    reg rst;
+    reg ebl;
+    reg dir;
+    reg [7:0] ctr;
+    reg [7:0] prev_ctr;
+    reg err;
 
-//Todo: User logic
-    
-//Todo: Finish test, check for success
 
-//Todo: Instantiate counter module
- 
-endmodule 
+    //Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+
+    //Todo: User logic
+
+    //check if reset works
+    initial begin
+      rst = 1
+      err = 0;
+      forever begin
+        #CLK_PERIOD
+        if ((rst == 1) && (ctr != 8'b0))
+        begin
+          $display("***TEST FAILED! reset is 1 but ctr is not 0");
+          err=1;
+        end
+        if(rst) rst = 0
+      end
+    end
+
+    //Todo: Finish test, check for success
+    initial begin
+      #50
+      if (err==0)
+        $display("***TEST PASSED! :) ***");
+      $finish;
+    end
+
+    //Todo: Instantiate counter module
+    counter top(
+        .rst (rst),
+        .enable (ebl),
+        .clk (clk),
+        .direction (dir),
+        .counter_out (ctr)
+        );
+
+endmodule
