33ee5ca4888f913131f9e6bc5059a0195fbbe867
DS-official <66016564+DS-official@users.noreply.github.com>
2020-06-09 12:17:46 +0100

    Ex4 v1

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,11 +1,11 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Exercise #4 
+// Exercise #4
 // Student Name:
-// Date: 
+// Date:
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
-//  a button is pressed, and stops when it is released. 
+//  a button is pressed, and stops when it is released.
 //
 //  inputs:
 //           clk, rst, button
@@ -16,3 +16,36 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module dice(
+    //Todo: add ports
+    input rst,
+    input clk,
+    input button,
+    output [2:0] throw
+    );
+
+    //Todo: add registers and wires, if needed
+    reg [2:0] throw;
+
+    //Todo: add user logic
+    always @ (posedge clk) begin
+    if(rst)
+        throw <= 3'b0;
+    else
+        case (throw)
+            3'b000: throw <= (button)?3'b001:3'b001;
+            3'b001: throw <= (button)?3'b010:3'b001;
+            3'b010: throw <= (button)?3'b011:3'b010;
+            3'b011: throw <= (button)?3'b100:3'b011;
+            3'b100: throw <= (button)?3'b101:3'b100;
+            3'b101: throw <= (button)?3'b110:3'b101;
+            3'b110: throw <= (button)?3'b001:3'b110;
+            3'b111: throw <= (button)?3'b001:3'b001;
+            default: throw <= 3'b0;
+        endcase
+
+
+
+    endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,66 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Electronic Dice
 // Student Name:
-// Date: 
+// Date:
 //
 // Description: A testbench module to test Ex4 - Electronic Dice
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+    //Todo: Parameters
+    parameter CLK_PERIOD = 10;
+
+    //Todo: Regitsers and wires
+    reg clk;
+    reg rst;
+    reg err;
+    reg btn;
+
+    wire [2:0] throw;
+    reg [2:0] prev_throw;
+
+
+
+    //Todo: Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+
+    //Todo: User logic
+    initial begin
+      err = 0;
+      #6
+      forever begin
+      
+        end
+    end
+
+
+
+
+    //Todo: Finish test, check for success
+    initial begin
+      #200
+      if (err==0)
+        $display("***TEST PASSED! :) ***");
+      $finish;
+    end
+
+    //Todo: Instantiate dice module
+    dice top(
+        .rst (rst),
+        .button (btn),
+        .clk (clk),
+        .throw (throw)
+        );
+
+endmodule
