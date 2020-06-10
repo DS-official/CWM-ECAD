7e6ddb74d2414a9199ee7d4c6cc51b0fd8b75f8e
Cloud User <centos@ip-172-31-23-22.ap-southeast-1.compute.internal>
2020-06-10 07:41:20 +0000

    Ex5 complete

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4
 // Student Name:Devang Sehgal
-// Date:9 June 2020
+// Date:9 June 2020 (but completed 10 June)
 //
 //  Description: In this exercise, you need to design an electronic dice, following
 //  the diagram provided in the exercises documentation. The dice rolls as long as
