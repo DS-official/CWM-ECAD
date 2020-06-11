20f18f05e668891aadd428536b59aac4e6d8e169
DS-official <66016564+DS-official@users.noreply.github.com>
2020-06-11 13:22:40 +0100

    Start 8

--- Ex8/top.v
+++ Ex8/top.v
@@ -1,14 +1,34 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #8
-// Student Name:
-// Date: 
+// Student Name: Devang Sehgal
+// Date: 11 June 2020
 //
 //  Description: In this exercise, you need to implement a times table of 0..7x0..7
 //  using a memory and AXI-4-lite interface.
 //
 //  inputs:
-//           clk, rst, a[2:0], b[2:0], read
+//           clk, rst, a[2:0], b[2:0], enable
 //
 //  outputs:
 //           result[5:0]
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns / 100ps
+
+module multiply_2(
+    //Todo: add ports
+    input clk,
+    input rst,
+    input [2:0] a,
+    input [2:0] b,
+    input enable,
+    output [5:0] result
+    );
+
+
+    //Todo: define registers and wires here
+
+
+
+
+endmodule
--- Ex8/top_tb.v
+++ Ex8/top_tb.v
@@ -1,9 +1,100 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #8 - Times Table using AXI interface
-// Student Name:
-// Date: 
+// Student Name: Devang Sehgal
+// Date: 11 June 2020
 //
 // Description: A testbench module to test Ex8 - Times Table using AXI4-Lite
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+    //Todo: Parameters
+    parameter CLK_PERIOD = 10;
+    reg err;
+    reg init;
+    reg clk;
+    reg rst;
+    reg [2:0] a;
+    reg [2:0] b;
+    reg read;
+    reg [3:0] count;
+
+    wire [5:0] result;
+    reg [5:0] prev_result;
+
+    //Todo: Clock generation
+    initial begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+
+
+
+     //Todo: User logic
+     initial begin
+       err = 0;
+       init = 0;
+       count = 0;
+       a = 1;
+       b = 2;
+       read = 0;
+       #CLK_PERIOD
+       forever begin
+       #30
+
+       //check if read works
+        if (init && (read==0) && (result != prev_result) )
+        begin
+          $display("***TEST FAILED! enable not working");
+          err=1;
+        end
+
+	//check for an arbitrary number
+	if (read && (a==3'd3) && (b == 3'd3) && (result != 6'd9))
+        begin
+          $display("***TEST FAILED! 3*3 is not coming as 9");
+          err=1;
+        end
+
+
+    init = 1;
+	count = count + 1;
+	a = a+1;
+	if (a == 0) b = b+1;
+	prev_result = result;
+    read = 1;
+	if(count == 4'b1010) read = 0;
+         end
+     end
+
+
+
+     //Todo: Finish test, check for success
+     initial begin
+       #500
+       if (err==0)
+         $display("***TEST PASSED! :) ***");
+       $finish;
+     end
+
+     // instantiate multiply module
+     multiply_2 top(
+         .clk (clk),
+         .rst (rst),
+         .a (a),
+         .b (b),
+         .read (read),
+         .result (result)
+         );
+
+
+
+
+
+endmodule
